f1f154deafb774b86475de86c028d0585d5bd231
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-09 09:20:21 +0530

    aws-vpc

---
+++ sg.tf
@@ -0,0 +1,10 @@
+module "mysql_sg" {
+  source = "../terraform-aws-sgroup"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_description = "instaces for mysql"
+  sg_name = mysql
+  project_name = var.project_name
+  environment = var.environment
+
+}
